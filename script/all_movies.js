4f2cf352ba94ca5bf44a7d26d1dfa16394a056e5
bj her <bj her>
2017-03-16 00:30:50 +0100

    la inn aadnes kode

--- script/all_movies.js
+++ script/all_movies.js
@@ -1,16 +1,14 @@
 window.onload = function() {
-    list_element = document.getElementById("all_movies");
+    var container = document.getElementById("sistTilgjengelig");
     for (movie_id in movies_object){
         movie_details = movies_object[movie_id];
-        list_item = document.createElement("LI");
-
-	// prepare the link
-        item_link = document.createElement("A");
-        item_link.href = "show_movie.html?id=" + movie_id;
-		link_text = document.createTextNode(movie_details["otitle"]);
-        item_link.appendChild(link_text);
-	
-        list_item.appendChild(item_link);
-        list_element.appendChild(list_item);      
+        var img = document.createElement("img");
+        img.src= "http://fillmurray.com/200/200";
+        var a = document.createElement("a");
+        a.href = "show_movie.html?id=" + movie_id;
+        a.appendChild(img);
+        container.appendChild(a);
+    // prepare the link
+              
     }
 }
