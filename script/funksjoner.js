17ee44fc01b304b5604e4b315eae4ba36d801764
adneforslund <adne.forslund@gmail.com>
2017-03-15 15:54:41 +0100

    implementert filmdatabasen

--- script/funksjoner.js
+++ script/funksjoner.js
@@ -3,11 +3,11 @@ var menyknapp = document.querySelector("#menyknapp")
 
 function toggle(){
 	if(this === sokeknapp){
-		document.querySelector(".sokebar").classList.toggle("gjemt")
+		document.querySelector(".sokebar").classList.toggle("gjemt");
 	}
 	if(this === menyknapp){
-		console.log("suka blyat sobaka")
-		document.querySelector("nav").classList.toggle("gjemt")
+		console.log("Y U NO WORK")
+		document.querySelector("nav").classList.toggle("gjemt");
 	}
 }
 
