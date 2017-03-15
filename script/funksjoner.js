4cacddc596acef441b28d36076fd76d7f2dc96fe
bj her <bj her>
2017-03-15 15:14:11 +0100

    menyknapp js funker

--- script/funksjoner.js
+++ script/funksjoner.js
@@ -3,13 +3,12 @@ var menyknapp = document.querySelector("#menyknapp")
 
 function toggle(){
 	if(this === sokeknapp){
-		document.querySelector(".sokebar").classList.toggle("gjemt")
+		document.querySelector(".sokebar").classList.toggle("gjemt");
 	}
 	if(this === menyknapp){
-		console.log("suka blyat sobaka")
-		document.querySelector("nav").classList.toggle("gjemt")
+		document.querySelector("nav").classList.toggle("gjemt");
 	}
 }
 
-sokeknapp.addEventListener("click", toggle)
-
+sokeknapp.addEventListener("click", toggle);
+menyknapp.addEventListener("click", toggle);
