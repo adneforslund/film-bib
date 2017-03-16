72d02b367b810601c01cc7d803024f4caeaa150e
Bjørnar Herland <kiy005@student.uib.no>
2017-03-16 01:04:16 +0100

    js genererer classer og id paa a-tag i allmoviesja

--- script/all_movies.js
+++ script/all_movies.js
@@ -6,6 +6,8 @@ window.onload = function() {
         img.src= "http://fillmurray.com/200/200";
         var a = document.createElement("a");
         a.href = "show_movie.html?id=" + movie_id;
+        a.id = "film" + movie_id;
+        a.className = "gjemt";
         a.appendChild(img);
         container.appendChild(a);
     // prepare the link
