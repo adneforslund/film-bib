2143639abaacfbf286303ae9f2165707cd83a956
Bjørnar Herland <kiy005@student.uib.no>
2017-03-22 02:21:56 +0100

    Fikset så man ikke loader 160mb med bilder

--- script/all_movies.js
+++ script/all_movies.js
@@ -1,11 +1,11 @@
 window.onload = function() {
    
-    addFilm("sistTilgjengelig");
-    addFilm("sistLant");
-    addFilm("ambefalinger");
+    addFilm("sistTilgjengelig",3);
+    addFilm("sistLant", 16);
+    addFilm("ambefalinger", 9);
 }
 
-function addFilm(where){
+function addFilm(where, start){
 
     var container = document.getElementById(where);
     addArrow(("#" + where) ,true);
@@ -13,14 +13,27 @@ function addFilm(where){
     for (movie_id in movies_object){
         movie_details = movies_object[movie_id];
         var img = document.createElement("img");
-        img.src= "http://fillmurray.com/200/200";
+        if (movie_id< (start+10)){
+        if (movie_id < 1000){
+            img.src= "https://nelson.uib.no/o/0/" + movie_id +".jpg";
+        }
+        else if (movie_id < 2000){
+            img.src= "https://nelson.uib.no/o/1/" + movie_id +".jpg";
+        }
+        else if (movie_id < 3000){
+            img.src= "https://nelson.uib.no/o/2/" + movie_id +".jpg";
+        }
+        }    
         var a = document.createElement("a");
         a.href = "show_movie.html?id=" + movie_id;
 
         a.id = "film" + movie_id;
         img.className = "filmBilde";
         img.alt = movie_details.etitle;
-        if (movie_id > 3){
+        if (movie_id > (start+2)){
+        a.className = "gjemt";
+        }
+        else if (movie_id < (start)){
         a.className = "gjemt";
         }
         a.appendChild(img);
