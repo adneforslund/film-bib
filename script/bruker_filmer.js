b086d39456dfd29e26a5a0d344a0b79cae6a3a0c
bj her <bj her>
2017-04-03 16:15:18 +0200

    klar for innlevering?

---
+++ script/bruker_filmer.js
@@ -0,0 +1,4 @@
+//skrevet av Bjørnar Herland/kiy005 
+
+object_onsker_laan={"brukernavn": [film_id, film_id], "brukernavn2": [film_id, film_id]};
+object_mine_laan={"brukernavn": [film_id ,film_id],"brukernavn2": [film_id ,film_id]};
