e6689078f138f518a23e88a866d7413ce4324e2a
bj her <bj her>
2017-04-21 15:05:29 +0200

    refaktorert bildehenting

--- script/funksjoner.js
+++ script/funksjoner.js
@@ -1,47 +1,47 @@
 var sokeknapp = document.querySelector("#sokeknapp");
 var menyknapp = document.querySelector("#menyknapp");
-function toggle(){
-	if(this === sokeknapp){
-		document.querySelector(".sokebar").classList.toggle("gjemt");
-	}
-	if(this === menyknapp){
-		document.querySelector("nav").classList.toggle("gjemt");
-	}
+
+function toggle() {
+    if (this === sokeknapp) {
+        document.querySelector(".sokebar").classList.toggle("gjemt");
+    }
+    if (this === menyknapp) {
+        document.querySelector("nav").classList.toggle("gjemt");
+    }
 }
 
-function snurr(){
+function snurr() {
 
-	var forelder = this.parentNode.getAttribute("id");
-	var barn = this.parentNode.children;
-	var start = NaN;
+    var forelder = this.parentNode.getAttribute("id");
+    var barn = this.parentNode.children;
+    var start = NaN;
 
-	if(this.classList.contains("pilhoyre")){
+    if (this.classList.contains("pilhoyre")) {
 
-		for(i=1; i<(barn.length-1); i++){
-			if(barn[i].classList.contains("gjemt")==false){
-				if(start != start){
-				start = i;
-				}
-			}
-		}
-		barn[start].classList.toggle("gjemt");
-		barn[start+3].classList.toggle("gjemt");
+        for (i = 1; i < (barn.length - 1); i++) {
+            if (barn[i].classList.contains("gjemt") == false) {
+                if (start != start) {
+                    start = i;
+                }
+            }
+        }
+        barn[start].classList.toggle("gjemt");
+        barn[start + 3].classList.toggle("gjemt");
 
-	}
-	else{
+    } else {
 
-		for(i=1; i<(barn.length-1); i++){
-			if(barn[i].classList.contains("gjemt")==false){
-				if(start != start){
-				start = i;
-				}
-			}
-		}
-		if(start>2){
-		barn[start+2].classList.toggle("gjemt");
-		barn[start-1].classList.toggle("gjemt");
-		}
-	}
+        for (i = 1; i < (barn.length - 1); i++) {
+            if (barn[i].classList.contains("gjemt") == false) {
+                if (start != start) {
+                    start = i;
+                }
+            }
+        }
+        if (start > 2) {
+            barn[start + 2].classList.toggle("gjemt");
+            barn[start - 1].classList.toggle("gjemt");
+        }
+    }
 }
 
 sokeknapp.addEventListener("click", toggle);
--- script/query_params.js
+++ script/query_params.js
@@ -21,20 +21,21 @@ https://tools.ietf.org/html/rfc3986#section-3.4
 
 function get_query_string_parameters() {
     result = {};
-    
+
     var uri = document.location.href;
-    
+
     if (uri.indexOf("?") < 0)
         return result;
     query_string = uri.substring(uri.indexOf("?"));
     if (query_string.indexOf("#") >= 0)
         query_string = query_string.substring(0, query_string.indexOf("#"));
 
-    
+
     query_pattern = /(?:[?&])([^&=]+)(?:=)([^&]*)/g;
-    decode = function(str) {return decodeURIComponent(str.replace(/\+/g, " "));};
-    while(match = query_pattern.exec(query_string))
+    decode = function(str) {
+        return decodeURIComponent(str.replace(/\+/g, " "));
+    };
+    while (match = query_pattern.exec(query_string))
         result[decode(match[1])] = decode(match[2]);
     return result;
 }
-
